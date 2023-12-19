eb6bb8cae175397196cc70718943bd65cbfbaed4
gia0901 <gianguyen.hcmut@gmail.com>
2023-12-19 23:35:00 +0700

    update showCPU, showRAM APIs

--- user_app.c
+++ user_app.c
@@ -7,11 +7,12 @@
 #include <errno.h>
 
 #define SSD1306_DEV_FILE    "/dev/ssd1306_lcd"
-
+#define MAX_CHAR_PER_LINE   21
 
 static void showMenu(void);
 static void showHost(void);
 static void showCPU(void);
+static void showRAM(void);
 static void printLCD(char *user_cmd);
 static void clearLCD(void);
 static void exitApp(void);
@@ -53,6 +54,10 @@ int main()
         {
             showCPU();
         }
+        else if ( !strcmp(temp_cmd, "showRAM"))
+        {
+            showRAM();
+        }
         else if ( !strcmp(temp_cmd, "print"))
         {
             printLCD(user_cmd);
@@ -79,9 +84,10 @@ static void showMenu(void)
     printf("Type the commands below to display information on the LCD:\n");
     printf("1. showHost         : show Host information\n");
     printf("2. showCPU          : show CPU information\n");
-    printf("3. print <string>   : print a string (max 40 characters) on the display\n");
-    printf("4. clear            : clear the display\n");
-    printf("5. exit             : exit the application\n");
+    printf("3. showRAM          : show RAM information\n");
+    printf("4. print <string>   : print a string (max 40 characters) on the display\n");
+    printf("5. clear            : clear the display\n");
+    printf("6. exit             : exit the application\n");
 }
 
 static void printLCD(char *user_cmd)
@@ -112,11 +118,6 @@ static void showHost(void)
     fp = popen("hostname", "r");
     fscanf(fp, "%s", temp_buf);
     sprintf(Name, "\nUser: %s\n", temp_buf);
-
-
-    printf("Host Name   : %s", Name);
-    printf("Host IP     : %s", IP);
-    printf("Host Kernel : %s", Kernel);
     
     clearLCD();
     write(fd, "--Host Information--\n", strlen("--Host Information--\n"));
@@ -127,8 +128,92 @@ static void showHost(void)
 
 static void showCPU()
 {
-    
+    char buff[50];
+    char *token;
+    char str[30];
+    FILE *fp;
+
+    clearLCD();
+    write(fd, "-- CPU Information --\n", strlen("-- CPU Information --\n"));
+
+    fp = popen("lscpu | grep CPU", "r");
+
+    while( fgets(buff, sizeof(buff), fp) )
+    {    
+        token = strtok(buff, ":");
+
+        if ( !strcmp("CPU(s)", token) )
+        {
+            token = strtok(NULL, " ");
+            sprintf(str, "\nNum of CPU(s): %s", token);
+        }
+        else if ( !strcmp("CPU max MHz", token) )
+        {
+            token = strtok(NULL, " ");
+            sprintf(str, "\nMax MHz: %s", token);
+        }
+        else if ( !strcmp("CPU min MHz", token) )
+        {
+            token = strtok(NULL, " ");
+            sprintf(str, "\nMin MHz: %s", token);
+        }
+
+        write(fd, str, strlen(str));
+        memset(str, 0, sizeof(str));
+    }   
 }
+
+
+static void showRAM()
+{
+    char buff[200];
+    char *temp_str[2];
+    char msg[3][MAX_CHAR_PER_LINE];
+    char *token;
+    int free_value;
+    int total_value;
+    int mem_fd;
+    int i;
+
+    clearLCD();
+
+    write(fd, "-- RAM Information --\n", strlen("-- RAM Information --\n"));
+
+    mem_fd = open("/proc/meminfo", O_RDONLY, 0666);
+
+    read(mem_fd, buff, sizeof(buff));
+
+    temp_str[0] = strtok(buff, "\n");
+    temp_str[1] = strtok(NULL, "\n");
+
+    for (i = 0; i < 2; i++)
+    {
+        token = strtok(temp_str[i], ":");
+
+        if ( !strcmp("MemTotal", token))
+        {
+            token = strtok(NULL, " ");
+            total_value = atoi(token);
+            sprintf(msg[0], "\nTotal: %d kB\n", total_value);
+        }
+        else if ( !strcmp("MemFree", token))
+        {
+            token = strtok(NULL, " ");
+            free_value = atoi(token);
+            sprintf(msg[2], "\nFree:  %d kB\n", free_value);
+        }
+    }
+
+    sprintf(msg[1], "\nUsed:  %d kB\n", total_value - free_value);
+    
+    for (int i = 0; i < 3; i++)
+    {
+        write(fd, msg[i], strlen(msg[i]));
+        memset(msg[i], 0, sizeof(msg[i]));
+    }
+
+}
+
 
 static void clearLCD()
 {
